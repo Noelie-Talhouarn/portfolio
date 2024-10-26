45e7355493e07d15e964d17123d683a43b0dbe06
Noelie-Talhouarn <noelie.talhouarn@edu.univ-fcomte.fr>
2024-10-26 23:49:42 +0200

    ajout d'un padding au border du footer

--- src/components/footerPage.vue
+++ src/components/footerPage.vue
@@ -7,7 +7,7 @@ import IconGithub from './icons/IconGithub.vue'
 </script>
 
 <template>
-  <footer class="px-28 lg:px-60">
+  <footer class="px-24 lg:px-60">
     <div class="border-t border-t-black p-2 pt-4">
       <nav
         class="gap-8 space-y-8 px-5 *:break-inside-avoid *:space-y-3  lg:col-start-3 lg:columns-3 lg:px-0 lg:flex lg:justify-center"
