d7ab5225f357a2a2974eef14a329a1882b13a4a2
Noelie-Talhouarn <noelie.talhouarn@edu.univ-fcomte.fr>
2024-10-27 12:29:38 +0100

    headerPage flottant

--- src/components/headerPage.vue
+++ src/components/headerPage.vue
@@ -14,14 +14,14 @@ function closeMenu() {
 </script>
 <template>
   <header
-    class="fixed top-0 left-0 right-0 flex items-center justify-between px-4 gap-8 h-20 bg-backgroundGris z-50"
+    class="fixed top-0 left-0 right-0 flex items-center justify-between px-4 gap-8 h-20 z-50"
     :class="{ 'bg-backgroundGris': activeMenu }"
   >
     <RouterLink to="/" aria-label="Accueil" @click="closeMenu">
       <IconLogo aria-hidden="true"  class="h-12 w-12" />
     </RouterLink>
     <button
-      class="relative z-10 flex h-5 w-8 flex-col justify-between lg:hidden *:block *:ease *:h-[2px] *:w-full *:transform *:rounded-full *:bg-black *:transition *:duration-300"
+      class="relative z-10 flex h-9 w-11 flex-col justify-between p-2 lg:hidden *:block *:ease *:h-[2px] *:w-full *:transform *:rounded-full *:bg-black *:transition *:duration-300 border border-mauve rounded-md bg-backgroundGris"
       @click="activeMenu = !activeMenu"
       aria-label="Menu"
       :aria-expanded="activeMenu ? 'true' : 'false'"
@@ -29,19 +29,19 @@ function closeMenu() {
       >
       <span :class="{ 'translate-y-[7px] rotate-45': activeMenu }"></span>
       <span :class="{ 'opacity-0': activeMenu }"></span>
-      <span :class="{ '-translate-y-[10px] -rotate-45 ': activeMenu }"></span>
-    </button>
+      <span :class="{ '-translate-y-[9px] -rotate-45 ': activeMenu }"></span>
+</button>
     <nav
       class="transform transition-transform duration-500 ease-in-out invisible opacity-0 flex-1 px-6 py-12 flex justify-between mt-20 flex-col fixed z-10 inset-0 bg-white lg:mt-0 lg:flex-row lg:static lg:bg-transparent lg:py-6 lg:visible lg:opacity-100 lg:justify-end"
       :class="{ '!visible !opacity-100 bg-gradient-to-b from-backgroundGris to-mauve': activeMenu }"
       v-scroll-lock="activeMenu"
     >
       <ul
-        class="flex flex-col gap-8 lg:flex-row text-2xl text-black text-center lg:text-xl lg:mr-8"
+        class="flex flex-col gap-8 lg:flex-row text-2xl text-black text-center lg:text-lg lg:px-4 lg:border lg:border-mauve lg:rounded-3xl lg:bg-backgroundGris "
       >
         <li>
           <RouterLink
-            class="block p-4 rounded-lg lg:hover:underline"
+            class="block py-2 rounded-lg lg:hover:underline"
             to="/architecte"
             @click="closeMenu"
             >Accueil</RouterLink
@@ -49,7 +49,7 @@ function closeMenu() {
         </li>
         <li>
           <RouterLink
-            class="block p-4 rounded-lg lg:hover:underline"
+            class="block py-2 rounded-lg lg:hover:underline"
             to="/batiment"
             @click="closeMenu"
             >Projets</RouterLink
@@ -57,7 +57,7 @@ function closeMenu() {
         </li>
         <li>
           <RouterLink
-            class="block p-4 rounded-lg lg:hover:underline"
+            class="block py-2 rounded-lg lg:hover:underline"
             to="/contact"
             @click="closeMenu"
             >Contact</RouterLink
@@ -65,27 +65,24 @@ function closeMenu() {
         </li>
         <li>
           <RouterLink
-            class="block p-4 rounded-lg lg:hover:underline"
+            class="block py-2 rounded-lg lg:hover:underline"
             to="/infos"
             @click="closeMenu"
             >A propos</RouterLink
           >
         </li>
-      <ul class="flex gap-12 justify-center">
+      <ul class="flex gap-12 justify-center lg:hidden">
         <li>
           <a href="https://github.com/Noelie-Talhouarn" aria-label="Github"
-            ><GithubIcon class="fill-white" aria-hidden="true"
-          /></a>
+            ><GithubIcon/></a>
         </li>
         <li>
           <a href="https://www.linkedin.com/in/noelie-talhouarn/" aria-label="Linkedin"
-            ><LinkedinIcon class="fill-white" aria-hidden="true"
-          /></a>
+            ><LinkedinIcon/></a>
         </li>
         <li>
           <a href="https://www.instagram.com/noelie.tl_/" aria-label="Instagram"
-            ><InstagramIcon class="fill-white" aria-hidden="true"
-          /></a>
+            ><InstagramIcon/></a>
         </li>
       </ul>
       </ul>
