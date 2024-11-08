79cdb919f73dd86d7e16e04d3c70044ea3e20178
Noelie-Talhouarn <noelie.talhouarn@edu.univ-fcomte.fr>
2024-11-08 17:55:38 +0100

    test img pocketbase

--- src/pages/projet/index.vue
+++ src/pages/projet/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { pb } from '@/backend';
+import ImgPb from '@/components/ImgPb.vue';
 
 const listProjet = await pb.collection('projets').getFullList()
 
@@ -8,6 +9,8 @@ const listProjet = await pb.collection('projets').getFullList()
 <template>
 
     <ul>
+            <img :src="ImgPb[0]" alt="photo de la maison" class="w-full lg:hidden" />
+
         <li v-for="unProjet in listProjet" :key="unProjet.id">{{ unProjet.nom_projet }}{{ unProjet.description_projet }}{{ unProjet.date_projet }}{{ unProjet.domaines1 }}{{ unProjet.domaines2 }}{{ unProjet.domaines3 }}{{ unProjet.img }}</li>
     </ul>
 </template>
