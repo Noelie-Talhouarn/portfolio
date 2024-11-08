28f3eec4fe61d66e259a2e4e475554c7c2a35bf7
Noelie-Talhouarn <noelie.talhouarn@edu.univ-fcomte.fr>
2024-11-08 10:14:46 +0100

    debut page d'accueil

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,22 +1,55 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-/* import Grille from '@/components/grille.vue'; */
+import Grille from '@/components/grille.vue'; 
 
+import Btn from '@/components/btn.vue'
 import { RouterLink, RouterView } from 'vue-router/auto'
-
-
 </script>
 
 <template>
-   
-   <!--  <Grille />
-    <div class="p-4">
+  <section>
+    
+  </section>
+  <Grille />
+    <!-- <div class="p-4">
     <Grid :columns="8" :rows="8" />
-  </div> -->
-
-
-
-
+  </div> --> 
+  <section class="p-8">
+    <section>
+      <article>
+        <h2>A propos de moi</h2>
+      </article>
+      <article>
+        <p>
+          Je suis Noélie et je suis étudiante en BUT du multimédia et de l’internet à Montbéliard.
+          Actuellement en deuxième année, je suis spécialisée dans le développement web.
+        </p>
+      </article>
+      <article>
+        <Btn url="/about">En savoir plus</Btn> 
+      </article>
+    </section>
+    <section>
+      <article>
+        <h2>Derniers projets</h2>
+      </article>
+      <article>carrousel</article>
+    </section>
+    <section>
+      <article>
+        <h2>On collabore ?</h2>
+      </article>
+      <article>
+        <p>
+          Besoin de renfort technique ? Je suis prête à contribuer à vos projets web, que ce soit
+          pour du développement front-end ou back-end. Contactez-moi pour transformer vos idées en
+          solutions web performantes !
+        </p>
+      </article>
+      <article>
+        <Btn url="/contact">C'est par ici</Btn> 
+      </article>
+    </section>
+  </section>
 </template>
-
 
