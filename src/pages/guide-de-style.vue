e2c2a06432ba04a05d14ea4865265f3af750917d
Noelie-Talhouarn <noelie.talhouarn@edu.univ-fcomte.fr>
2024-11-29 10:27:53 +0100

    suppression guide de style

--- src/pages/guide-de-style.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import Btn from '@/components/btn.vue'
-import CardProjet from '@/components/card-projet.vue'
-</script>
-<template>
-  <h1 class="font-ahsing text-7xl">guide de style</h1>
-  <Btn />
-  <CardProjet nom_projet="projet1" description_projet="lol" />
-</template>
