a9443f6d01ab612395a5b2710d02c90bcf622097
Noelie-Talhouarn <noelie.talhouarn@edu.univ-fcomte.fr>
2024-12-11 11:45:41 +0100

    changement typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ export default {
       },
       fontFamily: {
         'roboto-mono': 'Roboto Mono',
+        'spicy-rice': 'Spicy Rice',
         ahsing: 'Ahsing'
       },
       borderRadius: {
