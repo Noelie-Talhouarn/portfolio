1f63991bcbcd0e7d417f2c1c38ab29755fde332e
Noelie-Talhouarn <noelie.talhouarn@edu.univ-fcomte.fr>
2024-12-12 08:28:52 +0100

    page 404

---
+++ src/pages/[...page404].vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-8xl font-bold mb-4">404</h1>
+    <p class="text-2xl text-center">Désolé, la page que vous cherchez n'existe pas.</p>
+    <router-link to="/" class="mt-6 text-black hover:underline ">Retour à la page d'accueil</router-link>
+  </div>
+</template>
