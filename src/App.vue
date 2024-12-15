6b4cd5d32a78871c8e4bbf2a3709f5c85bfc4bb6
Noelie-Talhouarn <noelie.talhouarn@edu.univ-fcomte.fr>
2024-12-15 14:05:14 +0100

    ajout d'un composant pour le chargement

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,31 @@ import { RouterLink, RouterView } from 'vue-router/auto'
 import LogInOut from './components/LogInOut.vue';
 import HeaderPage from './components/headerPage.vue';
 import FooterPage from './components/footerPage.vue';
+
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import Loader from '@/components/loader.vue'
+
+// Variable pour suivre l'état de chargement
+const isLoading = ref(false)
+
+// Gestion des changements de route
+const router = useRouter()
+
+router.beforeEach((to, from, next) => {
+  isLoading.value = true // Démarre le loader
+  next()
+})
+
+router.afterEach(() => {
+  isLoading.value = false // Arrête le loader
+})
 </script>
 
 <template>
 <HeaderPage />
   <main class="pt-20">
+     <Loader v-if="isLoading" />
     <RouterView v-slot="{ Component }">
     <Suspense>
       <component :is="Component" :key="$route.path" />
---
+++ src/components/loader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="fixed inset-0 flex items-center justify-center bg-white z-50">
+    <div class="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+/* Optionnel : styliser l'arrière-plan */
+.bg-white {
+  background-color: rgba(255, 255, 255, 0.9);
+}
+</style>
